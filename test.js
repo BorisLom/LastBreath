22abbb90562039f97a9c8c3071aefdc5ee5a2bd1
teo laulan <teo.laulan@epitech.eu>
2022-02-24 21:58:59 +0100

    add project parsing

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 let text = [];
 let listOfContributors = [];
+let listOfProjects = [];
 
 
 
@@ -13,7 +14,7 @@ try {
 text = text.split('\n');
 const contributors = text[0][0];
 const projects = text[0][2];
-console.log(text);
+//console.log(text);
 
 class Contributor {
     constructor(name, skills, listOfSkills) {
@@ -35,7 +36,6 @@ class Project {
 }
 let i = 1;
 while ( listOfContributors.length < contributors) {
-    console.log(i);
     let listOfSkills = [];
     for (let j=0;j<text[i].split(' ')[1];j++) {
         let obj = {name:text[i+1+j].split(' ')[0].toString(), value:text[i+1+j].split(' ')[1]}
@@ -46,9 +46,21 @@ while ( listOfContributors.length < contributors) {
     i = i +1+ parseInt(text[i].split(' ')[1]);
 }
 
+while (listOfProjects.length < projects) {
+    let listOfSkills = [];
+    for (let j=0;j<text[i].split(' ')[4];j++) {
+        let obj = {name:text[i+1+j].split(' ')[0].toString(), value:text[i+1+j].split(' ')[1]}
+        listOfSkills.push(obj);
+    }
+    let project = new Project(text[i].split(' ')[0], text[i].split(' ')[1], text[i].split(' ')[2], text[i].split(' ')[3], text[i].split(' ')[4], listOfSkills);
+    listOfProjects.push(project);
+    i = i +1+ parseInt(text[i].split(' ')[4]);
+}
 
 
 
-console.log(listOfContributors);
+
+//console.log(listOfContributors);
+//console.log(listOfProjects);
 
 
